e72b0ce31a184d3c2a8cc21c365c7c261475d498
kali786516 <kali.tummala@gmail.com>
2015-12-22 23:45:00 +0000

    latest

---
+++ src/main/java/com/kali/latrobe/uni/spark/examples/coalescerepartition.scala
@@ -0,0 +1,36 @@
+package com.kali.latrobe.uni.spark.examples
+
+/**
+ * Created by kalit_000 on 22/12/2015.
+ */
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.{SparkContext, SparkConf}
+
+
+object coalescerepartition {
+
+ def main (args: Array[String]) {
+
+   Logger.getLogger("org").setLevel(Level.WARN)
+   Logger.getLogger("akka").setLevel(Level.WARN)
+
+   val conf = new SparkConf().setMaster("local[*]").setAppName("collasecereparition").set("spark.hadoop.validateOutputSpecs", "false")
+   val sc = new SparkContext(conf)
+
+   /* below y becomes 10 paritions ....*/
+
+   val y = sc.parallelize(1 to 10, 10)
+
+   /* y paritions are 10 */
+
+   println(y.partitions.length)
+
+   /*coaleasce will cut down to 2 paritions , false is used to say not to shuffle data ....*/
+
+   val z = y.coalesce(2, false)
+
+   println(z.partitions.length)
+
+  }
+}
