a548a837c3cf74348a4bc863b3d64ce4b6114690
kali786516 <kali.tummala@gmail.com>
2015-12-23 00:20:08 +0000

    latest

---
+++ src/main/java/com/kali/latrobe/uni/spark/examples/CollectAsMap.scala
@@ -0,0 +1,29 @@
+package com.kali.latrobe.uni.spark.examples
+
+/**
+ * Created by kalit_000 on 23/12/2015.
+ */
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.{SparkContext, SparkConf}
+
+object CollectAsMap {
+
+  def main (args: Array[String]) {
+
+    Logger.getLogger("org").setLevel(Level.WARN)
+    Logger.getLogger("akka").setLevel(Level.WARN)
+
+    val conf = new SparkConf().setMaster("local[*]").setAppName("collectasmap").set("spark.hadoop.validateOutputSpecs", "false")
+    val sc = new SparkContext(conf)
+
+    val a = sc.parallelize(List(1, 2, 1, 3), 1)
+    val b = a.zip(a)
+    b.collectAsMap
+
+
+
+
+  }
+
+}
