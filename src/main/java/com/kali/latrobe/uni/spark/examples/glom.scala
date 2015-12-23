be727104d1092584336e22a778db206c6bd8c407
kali786516 <kali.tummala@gmail.com>
2015-12-23 20:48:27 +0000

    latest

---
+++ src/main/java/com/kali/latrobe/uni/spark/examples/glom.scala
@@ -0,0 +1,26 @@
+package com.kali.latrobe.uni.spark.examples
+
+/**
+ * Created by kalit_000 on 23/12/2015.
+ */
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.{SparkContext, SparkConf}
+
+object glom {
+
+  def main(args: Array[String]) {
+
+    Logger.getLogger("org").setLevel(Level.WARN)
+    Logger.getLogger("akka").setLevel(Level.WARN)
+
+    val conf = new SparkConf().setMaster("local[*]").setAppName("YOUR_APP_NAME_USER").set("spark.hadoop.validateOutputSpecs", "false")
+    val sc = new SparkContext(conf)
+
+
+    val a = sc.parallelize(1 to 100, 3)
+    a.glom.collect.foreach(println)
+  }
+
+
+}
