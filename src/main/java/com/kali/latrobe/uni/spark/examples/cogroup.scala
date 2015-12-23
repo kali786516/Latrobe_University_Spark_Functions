c577880701da8f35276c9368130ac2bdeacd8969
kali786516 <kali.tummala@gmail.com>
2015-12-23 00:08:26 +0000

    latest

--- src/main/java/com/kali/latrobe/uni/spark/examples/cogroup.scala
+++ src/main/java/com/kali/latrobe/uni/spark/examples/cogroup.scala
@@ -23,6 +23,11 @@ object cogroup {
     val c = a.map((_, "c"))
 
     /*
+    Avoid the flatMap-join-groupBy pattern. When two datasets are already grouped by key and you want to join them and keep them grouped,
+    you can just use cogroup. That avoids all the overhead associated with unpacking and repacking the groups.
+     */
+
+    /*
     RDD b data:-
     (1,b)
     (2,b)
